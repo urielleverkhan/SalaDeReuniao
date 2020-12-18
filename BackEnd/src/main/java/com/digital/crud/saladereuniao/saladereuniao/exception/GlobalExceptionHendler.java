acea1226c0846ba41188b7629be6efaef795987c
Naira Magalhães <52166063+urielleverkhan@users.noreply.github.com>
2020-12-18 01:58:43 -0300

    - BackEnd da aplicação pronto

---
+++ BackEnd/src/main/java/com/digital/crud/saladereuniao/saladereuniao/exception/GlobalExceptionHendler.java
@@ -0,0 +1,27 @@
+package com.digital.crud.saladereuniao.saladereuniao.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHendler {
+
+    @ExceptionHandler(ResourceNotFoundExeption.class)
+    public ResponseEntity<?> resouceNotFoundExeption(ResourceNotFoundExeption ex, WebRequest request){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?>globalExceptionHandler(Exception ex, WebRequest request){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+}
